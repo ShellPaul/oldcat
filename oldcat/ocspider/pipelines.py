8dca81269fbc3f90a6a898cb911b522353f02780
shellpaul <shellpaul@foxmail.com>
2016-07-31 09:50:40 +0800

    no exception when same url save

--- oldcat/ocspider/pipelines.py
+++ oldcat/ocspider/pipelines.py
@@ -5,15 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import logging
+
 from ..models.news import News
+
+logger = logging.getLogger(__name__)
 
 
 class NewsPipeline(object):
 
     def process_item(self, item, spider):
-        news = News()
-        news.frm = item['url'].encode("utf-8", "ignore")
-        news.title = item['title'].encode("utf-8", "ignore")
-        news.article = item['content'].encode("utf-8", "ignore")
-        news.update_time = item['created'].encode("utf-8", "ignore")
-        news.save()
+        frm = item['url'].encode("utf-8", "ignore")
+        title = item['title'].encode("utf-8", "ignore")
+        article = item['content'].encode("utf-8", "ignore")
+        update_time = item['created'].encode("utf-8", "ignore")
+        news, created = News.get_or_create(frm=frm, defaults={
+            "title": title,
+            "article": article,
+            "update_time": update_time,
+        })
+        logger.info("[created: %s] %s" % (created, news.frm))
